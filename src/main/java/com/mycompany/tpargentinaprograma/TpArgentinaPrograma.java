34f8c8bc6b4db7ff0b565b1e59a89c506e58ee86
Valentin Meier <valentinmeier123@gmail.com>
2023-03-19 19:52:53 -0300

    Inicio

---
+++ src/main/java/com/mycompany/tpargentinaprograma/TpArgentinaPrograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpargentinaprograma;
+
+/**
+ *
+ * @author Meier
+ */
+public class TpArgentinaPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
